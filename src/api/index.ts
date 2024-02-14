4fb1c7c5e7ca8d30127dc6702f3e55c003cf1c5a
Toi Yoshimaru <0maru.dev@gmail.com>
2024-02-14 22:23:17 +0900

    不要なコードを削除

--- src/api/index.ts
+++ src/api/index.ts
@@ -13,34 +13,3 @@ api.doc("/openapi", {
     },
 })
 api.route("/contact", contactApi)
-
-
-function swaggerUI(arg0: { url: string; }): import("hono/types").H<import("hono").Env, "/ui", import("hono/types").BlankInput, any> {
-    throw new Error("Function not implemented.");
-}
-// const route = createRoute({
-//   path: '/contact',
-//   method: 'post',
-//   description: 'お問い合わせAPI',
-//   responses: {
-//     200: {
-//       description: '成功',
-//       content: {
-//         'application/json': {
-//           schema: {
-//             type: 'object',
-//             properties: {
-//               message: {
-//                 type: 'string'
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// })
-
-// const handler: RouteHandler<typeof route> = (c) => {
-//   return c.json({ message: 'お問い合わせを受け付けました' })
-// }
